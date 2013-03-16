a99a4b102a890596818e107f02090576c915d569
Dread Quixadhal <quixadhal@gmail.com>
2013-03-16 03:49:24 -0400

    Removed windows junk, renamed fluffos directory

--- win32/mud.bat
+++
@@ -1,7 +0,0 @@
-# This improved batch file courtesy of Brodbane @ Eve
-goto start
-:start
-c:\lil\win32\driver.exe c:\lil\bin\mudos.cfg.win32
-# Add a '#' to the following line to prevent auto-restarts
-goto start
-:end
